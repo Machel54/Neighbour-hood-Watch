fdcd1b94be4a6a5bdbc9a6801b236c709b4935af
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-12-02 08:17:30 +0300

    Create admin model

--- hood/settings.py
+++ hood/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'neighbour.apps.NeighbourConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
+        'USER': 'machel',
+    'PASSWORD':'Nyanumba2001',
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_admin(self):
+        self.save()
+        
+class User(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
+
+    def save_user(self):
+        self.save()
