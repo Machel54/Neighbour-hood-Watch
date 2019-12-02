6d12be06513e6a9fc5a1db6ff0d935c2d7685405
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-12-02 21:27:13 +0300

    Create profile view and url

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$', views.index, name='index'),
+    url(r'^profile/',views.profile, name='profile'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^create_profile/', views.create_profile, name='create_profile'),
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- neighbour/views.py
+++ neighbour/views.py
@@ -18,3 +18,21 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/login')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/registration_form.html', {"form": form})
+
+@login_required(login_url='/login')
+def profile(request):
+    user = request.user
+    context = {
+        "user": user,
+    }
+    return render(request, 'profile.html', context)
+
