b630cd8e5386ee90df9d0da48d26bb085e98f2df
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-12-02 10:54:45 +0300

    Create registartion and login for user

--- hood/settings.py
+++ hood/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bootstrap4',
     'neighbour.apps.NeighbourConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Admin,User
+from .models import Admin,User,Hood
 
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(User)
-# admin.site.register(Author)
+admin.site.register(Hood)
