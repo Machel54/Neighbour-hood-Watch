6b5a8fecf4382392f98dd2b677993b6bc27e46ea
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-12-02 15:32:09 +0300

    Create model Mwenyeji

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime as dt
+# from django.contrib.auth.models import User
 
 # Create your models here.
 # class Admin(models.Model):
@@ -34,11 +35,18 @@ class User(models.Model):
 
 #     def save_author(self):
 #         self.save()
-        
+ class Mwenyeji(models.Model):
+    first_name = models.CharField(max_length =30)     
+    def __str__(self):
+        return self.name
+
+    def save_mwenyeji(self):
+        self.save()
+     
 class Hood(models.Model):
     name = models.CharField(max_length=20)
     location = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='hood_photo', blank=True, default='hood_photo/hood.jpg')
+    image = models.ImageField(upload_to='hood_photo', blank=True, default='hood/hood.jpg')
     pub_date = models.DateTimeField(auto_now_add=True)
     admin = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -47,7 +55,7 @@ class Hood(models.Model):
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_photo', blank=True, default='profile_photo/defaultprofile.jpg')
+    image = models.ImageField(upload_to='profile_photo', blank=True, default='profile_photo/profile.jpg')
     bio = models.CharField(max_length=255, blank=True)
     contacts = models.CharField(max_length=200)
     join_date = models.DateTimeField(auto_now_add=True)
@@ -55,4 +63,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+# class Establishment(models.Model):
+#     name = models.CharField(max_length=20)
+#     location = models.CharField(max_length=20)
+#     contact = models.CharField(max_length=30)
+#     image = models.ImageField(upload_to='facilities', blank=True, default='establishment/sample.jpg')
+#     pub_date = models.DateTimeField(auto_now_add=True)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     hood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f'{self.name}'
     
