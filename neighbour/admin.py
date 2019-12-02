60ed6969eeed1e2f8a841bcb2b9ef8459fd73b98
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-12-02 09:34:30 +0300

    Create Hood modeland delete model Author

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -4,3 +4,4 @@ from .models import Admin,User
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(User)
+# admin.site.register(Author)
--- neighbour/models.py
+++ neighbour/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Admin(models.Model):
@@ -23,3 +24,23 @@ class User(models.Model):
     def save_user(self):
         self.save()
         
+# class Author(models.Model):
+#     first_name = models.CharField(max_length =30)
+#     last_name = models.CharField(max_length =30)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return self.author
+
+#     def save_author(self):
+#         self.save()
+        
+class Hood(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='hood_photo', blank=True, default='hood_photo/hood.jpg')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
